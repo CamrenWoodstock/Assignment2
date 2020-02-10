cdb6efa43a539421a548666ea1b8d831d896debc
Camren Woodstock <47184405+CamrenWoodstock@users.noreply.github.com>
2020-02-10 14:15:39 -0500

    Update uarray2.c

--- uarray2.c
+++ uarray2.c
@@ -1,6 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "uarray2.h"
+#include "uarray.h"
+#include "assert.h"
+#include "mem.h"
 
-
-UArray2_T UArray2_new(int height, int width, int size) {
+extern UArray2_T UArray2_new(int height, int width, int size) {
   UArray2_T grid;
   NEW(grid);
   //Initialize the Grid
@@ -16,14 +21,14 @@ UArray2_T UArray2_new(int height, int width, int size) {
   }
   return grid;
 }
-
-void UArray2_free(UArray2_T *grid) {
+                
+extern void UArray2_free(UArray2_T *grid) {
   assert(*grid);
   for (int i = 0; i < height; i++) Array_free(Array_get(grid->rows, i)); 
   Array_free(grid->rows);
 }
 
-void* UArray2_at(UArray2_T grid, int height, int width) {
+extern void* UArray2_at(UArray2_T grid, int height, int width) {
   assert(grid);
   assert(height >= 0 && height < grid->height);
   assert(width >= 0 && width < grid->width);
@@ -31,16 +36,27 @@ void* UArray2_at(UArray2_T grid, int height, int width) {
   return Array_get(row, width);
 }
 
-int UArray2_height(UArray2_T grid) {
+extern int UArray2_height(UArray2_T grid) {
   assert(grid);
   return grid->height;
 }
 
-int UArray2_width(UArray2_T grid) {
+extern int UArray2_width(UArray2_T grid) {
   assert(grid);
   return grid->width;
 }
 
-int UArray2_size(UArray2_T grid) {
+extern int UArray2_size(UArray2_T grid) {
   assert(grid);
   return grid->size;
+}
+                
+extern void UArray2_map_col_major(UArray2_T grid,
+apply(int xIndex, int yIndex, UArray2_T grid, void *indexPtr, void *cl), void *cl); {
+  
+}
+                            
+extern void UArray2_map_row_major(UArray2_T grid, 
+apply(int xIndex, int yIndex, Uarray2_T grid, void *indexPtr, void *cl), void *cl); {
+  
+}
