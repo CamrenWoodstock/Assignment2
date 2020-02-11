b8b5f306e700b501189338bb1a59657614fc8077
Camren Woodstock <47184405+CamrenWoodstock@users.noreply.github.com>
2020-02-11 00:33:25 -0500

    Update uarray2.h

--- uarray2.h
+++ uarray2.h
@@ -3,17 +3,23 @@
 #include "array.h"
 #define T UArray2_T
 
-typedef  struct T *T;
+
+typedef struct T {
+  int height;
+  int width;
+  int size;
+  UArray_T rows;
+} 
 extern T UArray2_new(int length, int width, int size);
-extern void UArray2_free(T *UArray2);
-extern int UArray2_length(T UArray2);
-extern int UArray2_width(T UArray2);
-extern int UArray2_size(T UArray2);
-extern void *UArray2_at(T UArray2, int x, int y);
-extern void UArray2_map_col_major(T UArray2,
-apply(int xIndex, int yIndex, T UArray2, void *indexPtr, void *cl), void *cl);
-extern void UArray2_map_row_major(T UArray2, 
-apply(int xIndex, int yIndex, T UArray2, void *indexPtr, void *cl), void *cl);
+extern void UArray2_free(T *grid);
+extern int UArray2_length(T grid);
+extern int UArray2_width(T grid);
+extern int UArray2_size(T grid);
+extern void *UArray2_at(T grid, int x, int y);
+extern void UArray2_map_col_major(T grid,
+apply(int xIndex, int yIndex, T grid, void *indexPtr, void *cl), void *cl);
+extern void UArray2_map_row_major(T grid, 
+apply(int xIndex, int yIndex, T grid, void *indexPtr, void *cl), void *cl);
 
 #undef T
 #endif
